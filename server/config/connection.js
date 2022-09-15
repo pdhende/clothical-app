c4597217e3b79cf41ed9bfe82961e58b47da0682
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-09-15 00:47:09 -0400

    Set up Sequelize connection to database, and syncing of models. Models need to be built. Removed .env file from Git tracking.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,19 +1,22 @@
 // Import and configure dotenv
 require('dotenv').config();
-// Import and require mysql2
-const mysql = require('mysql2');
-// Connect to database; the location of the .env file
-// must be in the same parent folder as the execution
-// of the server/index.js file
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  },
-  console.log(`Connected to the ${process.env.DB_NAME} database.
-*************************************************`)
-);
 
-module.exports = db;
+// Main class and entry point to Sequelize
+const Sequelize = require('sequelize');
+
+// Call Sequelize constructor and instantiate new sequelize object
+// with name of database, username, and password; the location of 
+// the .env file  must be in the same parent folder as the execution
+// of the server/index.js file
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
+
+// test if the Sequelize connection to database is OK
+sequelize.authenticate()
+  .then(() => console.log('Sequelize connection to database is successfully established.'))
+  .catch(err => console.error('Sequelize connection to database failed:', err));
+
+module.exports = sequelize;
--- server/index.js
+++ server/index.js
@@ -1,28 +1,25 @@
-const db = require('./config/connection');
-// console.log('db is', db);
+const express = require("express");
+const sequelize = require("./config/connection");
 
-// Just testing connection to database and the ability
-// to execute an SQL command here...
-db.query(`SELECT * FROM customer`, function(err, results) {
-    console.log('Results from database are', results);
-})
-
-const express = require('express');
-
-const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const PORT = process.env.PORT || 3001;
 
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+// necessary middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Use apiRoutes
 // app.use('/api', apiRoutes);
 // app.use('/', htmlRoutes);
 
-app.listen(PORT, () => {
-  console.log(`API server now listening on port ${PORT}!`);
-  console.log('*****************************************');
+// sync all the models with the database
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`API server now listening on port ${PORT}!`);
+    console.log("*****************************************");
+  });
 });
