743bfb5a6c42880f26ec9f2b3241c355cfbeae17
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-09-16 12:14:52 -0400

    added .env file and added the user model

---
+++ server/models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('sequelize');
+const bcrypt = require('bcrypt');
+// const Order = require('./Order');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  firstName: {
+
+  },
+  lastName: {
+
+  },
+  telephone: {
+
+  },
+  dateRegistered: {
+  
+  }
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
